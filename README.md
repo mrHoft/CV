d1c14005e6a5068508aac61d6d5b0d8f9dace8d9
mrHoft <ru.hoft@gmail.com>
2025-01-03 13:38:32 +0300

    feat: migrate enire project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-[rsschool-cv](https://mrHoft.github.io/rsschool-cv/cv)<br/>
-[rsschool-cv-html](https://mrhoft.github.io/rsschool-cv/)<br/>
+<div align="center">
+
+  [![node](https://img.shields.io/badge/deno-2.1-blue?logo=deno)](#)
+  [![vite](https://img.shields.io/badge/vite-6.0-blue?logo=vite)](#)
+  [![react](https://img.shields.io/badge/vue-3.5-blue?logo=vuedotjs)](#)
+
+</div>
+
+[deploy](https://mrhoft.github.io/cv/)<br/>
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,40 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
+import { Navigation, Info, About, Skills, Experience, Education, Codeexample } from './article'
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <Navigation />
+  <div class="content">
+    <main>
+      <Info />
+      <About />
+      <Skills />
+      <Experience />
+      <Education />
+      <Codeexample />
+    </main>
+    <footer class="footer">
+      <span>© 2025 <a href="https://github.com/mrHoft">mrHoft</a></span>
+    </footer>
+  </div>
 </template>
 
 <style scoped>
+.content {
+  width: 100%;
+}
+
+footer {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 1em;
+  width: 100%;
+  background-color: var(--color20);
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+footer img {
+  filter: invert(var(--color-scheme));
+}
 </style>
